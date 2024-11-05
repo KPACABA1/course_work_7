6266c76e77804db6e3f72cd685503da283e16b4e
groni <groninger.official@yandex.ru>
2024-11-06 03:22:39 +0500

    Поправил переменные окружения

--- config/settings.py
+++ config/settings.py
@@ -141,8 +141,8 @@ CSRF_TRUSTED_ORIGINS = [
 CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 
 # Настройка для Celery-beat
 CELERY_BEAT_SCHEDULER='django_celery_beat.schedulers:DatabaseScheduler'
